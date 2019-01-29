7181e6a8803158acdf1bb3a1c716ec2d187c2c53
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2019-01-29 17:14:20 +0700

    add inference class

---
+++ PKDSS/PKDSS.MonoApp/Helper/SoilNutritionModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKDSS.MonoApp.Helper
+{
+    public class SoilNutritionModel
+    {
+        const string BASE_PYTHON = @"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\python.exe";
+        readonly string CMD = ConfigurationManager.AppSettings["PythonSript"];
+        public string InferenceModel(string FileCSV)
+        {
+            try
+            {
+                var args = $"{FileCSV}";
+                Console.WriteLine(args);
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = BASE_PYTHON;//"python";
+                start.Arguments = string.Format("{0} {1}", CMD, args);
+                start.UseShellExecute = false;
+                start.CreateNoWindow = true;
+                start.RedirectStandardOutput = true;
+                start.RedirectStandardError = true;
+                using (Process process = Process.Start(start))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = reader.ReadToEnd();
+                        Console.Write(result);
+                        return result;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Execute python failed:"+ex.Message);
+            }
+            return "";
+        }
+    }
+}
