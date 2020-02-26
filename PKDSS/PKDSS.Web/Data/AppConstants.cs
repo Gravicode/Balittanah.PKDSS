68bd83ed5e99dfb7dc8cc98c77bfa2f38abef604
Gravicode Multinovative Plexindo <mifmasterz@yahoo.com>
2020-02-26 14:34:14 +0700

    add map

---
+++ PKDSS/PKDSS.Web/Data/AppConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PKDSS.Web.Data
+{
+    public class AppConstants
+    {
+        public static string WebRootPath { get; set; }
+        public static string GMapApiKey { set; get; }
+    }
+}
